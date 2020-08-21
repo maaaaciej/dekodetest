102df5f441fec93d6837610465e0f383a90890f0
Maciej <ostromaciej@gmail.com>
2020-08-21 14:49:05 +0200

    added header styling, debating on wether or not to pull the search-field into its own component

--- src/components/Header/header.component.jsx
+++ src/components/Header/header.component.jsx
@@ -1,41 +1,50 @@
 import React from "react";
 
-import { ReactComponent as Logo } from "../../assets/logo/logo.svg";
+import logo from "../../assets/logo/logo.svg";
+import search from "../../assets/search/search.svg";
 
 import "./header.styles.scss";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header-logo">
-        <Logo />
+    <header className="header">
+      <div className="header-container">
+        <nav className="header-container-nav">
+          <a href="https://www.dekode.no" className="header-container-nav-logo">
+            <img src={logo} alt="logo" />
+          </a>
+          <a
+            href="https://dekode.no/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="header-container-nav-link"
+          >
+            Circles
+          </a>
+          <a
+            href="https://dekode.no/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="header-container-nav-link"
+          >
+            Squares
+          </a>
+          <a
+            href="https://dekode.no/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="header-container-nav-link"
+          >
+            Triangles
+          </a>
+        </nav>
+
+        <span className="header-container-search">
+          Search{" "}
+          <img className="header-container-search-icon" src={search} alt="" />
+        </span>
       </div>
-      <a
-        href="https://dekode.no/"
-        rel="noopener noreferrer"
-        target="_blank"
-        className="header-link"
-      >
-        Circles
-      </a>
-      <a
-        href="https://dekode.no/"
-        rel="noopener noreferrer"
-        target="_blank"
-        className="header-link"
-      >
-        Squares
-      </a>
-      <a
-        href="https://dekode.no/"
-        rel="noopener noreferrer"
-        target="_blank"
-        className="header-link"
-      >
-        Triangles
-      </a>
-      <input type="text" placeholder="Search" className="header-search" />
-    </div>
+    </header>
   );
 };
 

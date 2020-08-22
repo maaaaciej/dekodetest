dfbf2ac7667f9176eb56f8120dc55aec40cc859d
Maciej <ostromaciej@gmail.com>
2020-08-23 00:22:08 +0200

    set up testing infrastructure

---
+++ App.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+describe("<App />", () => {
+  it("Renders <App /> component correctly", () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Purple Circles/i)).toBeInTheDocument();
+  });
+});
---
+++ src/pages/PostContainer/postcontainer.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import PostContainer from "./postcontainer.component";
+
+it("works", () => {
+  return;
+});
